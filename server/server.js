f2532623e260cf8e4d792e9e8bffb3816ee62576
Prakash-305 <udhayaprakash1411@gmail.com>
2025-05-23 21:34:42 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
+// Middlewares
 app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
